6d79c29aca1cb8425e0d2d5e6da3844575a62097
[dyad] <puzon.304620@caloocan.sti.edu.ph>
2026-05-14 22:20:18 +0800

    [dyad] Fixed signature page-syncing and color shift issues by isolating signatures per page and ensuring transparency. - wrote 4 file(s) + extra files edited outside of Dyad

--- statemobile/StateMobile/Models/ScannedDocument.cs
+++ statemobile/StateMobile/Models/ScannedDocument.cs
@@ -1,26 +1,26 @@
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace StateMobile.Models
 {
     public class ScannedDocument
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
-        public List<ScannedPage> Pages { get; set; } = new();
-        public string Title { get; set; }
+        public string? Title { get; set; }
+        public ObservableCollection<ScannedPage> Pages { get; set; } = new();
     }
 
     public class ScannedPage
     {
-        public string ImagePath { get; set; }
-        public SignaturePlacement Signature { get; set; }
+        public int PageNumber { get; set; }
+        public string ImagePath { get; set; } = string.Empty;
+        public PageSignature? Signature { get; set; }
     }
 
-    public class SignaturePlacement
+    public class PageSignature
     {
-        public byte[] SignatureData { get; set; }
+        public byte[]? SignatureData { get; set; }
         public double X { get; set; }
         public double Y { get; set; }
-        public double Width { get; set; }
-        public double Height { get; set; }
+        public double Width { get; set; } = 150;
+        public double Height { get; set; } = 75;
     }
 }
